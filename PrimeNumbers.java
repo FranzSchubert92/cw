aba894199090af353e10024c203381a15c276444
Eric Heintzberger <eric.heintzberger@gmail.com>
2017-08-16 13:07:13 -0700

    added  explanation for isProbablePrime() in PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -14,3 +14,23 @@ public class PrimeNumbers {
     }
 
 }
+
+/*
+ * java.math.BigInteger.isProbablePrime(int certainty) returns true if this 
+ * BigInteger is probably prime, false if it's definitely composite. If 
+ * certainty is ≤ 0, true is returned.
+ *
+ * Declaration
+ * Following is the declaration for java.math.BigInteger.isProbablePrime() method.
+ *
+ * public boolean isProbablePrime(int certainty)
+ *
+ * Parameters
+ * certainty − a measure of the uncertainty that the caller is willing to 
+ * tolerate: if the call returns true the probability that this BigInteger 
+ * is prime exceeds (1 - 1/(2**certainty)). The execution time of this method 
+ * is proportional to the value of this parameter.
+ *
+ * Return Value
+ * This method returns true if this BigInteger is probably prime, false if it's definitely composite.
+ */
